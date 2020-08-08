dff7efc0bc3ee9b0e43411f021ce51efe222935d
James Kao <james.l.kao@gmail.com>
2020-08-08 15:26:48 -0700

    remove markdown

--- Package.swift
+++ Package.swift
@@ -19,19 +19,6 @@ let package = Package(
         "YZDesignSystemMacOS",
         "YZImage",
     ]),
-    // TODO: MacOS only
-    .library(
-      name: "YZMarkdown",
-      targets: [
-        "YZMarkdown",
-    ]),
-  ],
-  dependencies: [
-    // Upstream
-    .package(url: "http://github.com/jdfergason/swift-toml", from: "1.0.0"),
-//    .package(url: "https://github.com/iwasrobbed/Down", from: "0.9.3"),
-    // Local
-    .package(path: "ThirdParty/Down")
   ],
   targets: [
     .target(
@@ -65,22 +52,6 @@ let package = Package(
         "EnvironmentValues+ImageCache.swift",
         "ImageCache.swift",
         "ImageLoader.swift",
-      ]
-    ),
-    .target(
-      name: "YZMarkdown",
-      dependencies: [
-        "Toml",
-        "Down",
-        "YZImage",
-        "YZDesignSystem",
-      ],
-      path: "Sources/YZMarkdown",
-      sources: [
-        "YZMarkdownParser.swift",
-        "YZMarkdownView.swift",
-        "Renderer.swift",
-        "YZMarkdownDebugRenderer.swift",
       ]
     ),
     .testTarget(
