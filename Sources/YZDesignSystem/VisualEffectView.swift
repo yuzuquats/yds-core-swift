d3628b00b69520eda657445b0c65cdc96017bd8b
James Kao <james.l.kao@gmail.com>
2020-08-08 18:29:15 -0700

    Fix VisualEffectView

--- Sources/YZDesignSystem/VisualEffectView.swift
+++ Sources/YZDesignSystem/VisualEffectView.swift
@@ -2,10 +2,23 @@ import Foundation
 import AppKit
 import SwiftUI
 
-public struct VisualEffectView: NSViewRepresentable {
+public struct VisualEffectView: NSViewRepresentable
+{
   public typealias NSViewType = NSVisualEffectView
   
-  var material: NSVisualEffectView.Material?
-  public func makeNSView(context: NSViewRepresentableContext<Self>) -> NSVisualEffectView { NSVisualEffectView() }
-  public func updateNSView(_ nsView: NSVisualEffectView, context: NSViewRepresentableContext<Self>) { nsView.material = material! }
+  var material: NSVisualEffectView.Material
+  
+  public init(material: NSVisualEffectView.Material) {
+    self.material = material
+  }
+  
+  public func makeNSView(context: NSViewRepresentableContext<Self>) -> NSVisualEffectView
+  {
+    NSVisualEffectView()
+  }
+  
+  public func updateNSView(_ nsView: NSVisualEffectView, context: NSViewRepresentableContext<Self>)
+  {
+    nsView.material = material
+  }
 }
